001ddd7f87139cdfdb2a4f7745e36f2196d0211c
linrz <linrz@lastwhisper.cn>
2019-03-10 21:34:13 +0800

    feature: upgrade vuepress-theme-cute

--- service-worker.js
+++ service-worker.js
@@ -21,23 +21,23 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "2017/11/05/fastify/index.html",
-    "revision": "8a5f06c5ebd21a2eeaa6dab6bd86189f"
+    "revision": "70a78ba416ff210aaf698a29d179d7f9"
   },
   {
     "url": "2018/05/15/why-i-unlike-styled-components/index.html",
-    "revision": "959ff5b1402f128ade839db1db0f4694"
+    "revision": "1cf74b42f826adbe62b60f98d99ec16a"
   },
   {
     "url": "2019/01/12/pnp/index.html",
-    "revision": "0e25defbfb85f650abcbce304d938a87"
+    "revision": "d41542b6034fef5a176ba25da6d83cb7"
   },
   {
     "url": "404.html",
-    "revision": "ed972dd77961eb054b088429f2e18a21"
+    "revision": "218dcfac1400928bfa43ad232b7f27a7"
   },
   {
     "url": "about.html",
-    "revision": "27c855062b8956063831b11c5092cc5f"
+    "revision": "ed9c57bf3c21665aaef4add20c4bd7b2"
   },
   {
     "url": "assets/css/0.styles.fa24b528.css",
@@ -48,12 +48,12 @@ self.__precacheManifest = [
     "revision": "9c41a2b5180354bffb6fe3a759b8aab4"
   },
   {
-    "url": "assets/js/10.37817de7.js",
-    "revision": "106cf2fb236b1aece1e3bdbfaa0d93f0"
+    "url": "assets/js/10.08d8bf5e.js",
+    "revision": "cb63d6552f2033d1cba147c6a027ea2d"
   },
   {
-    "url": "assets/js/11.6e77b334.js",
-    "revision": "853425358368c13999213acdd1eeebe3"
+    "url": "assets/js/11.e8d04f16.js",
+    "revision": "cdd59e6c8f54fb54f82cd2f2c38ee420"
   },
   {
     "url": "assets/js/12.8324e8ad.js",
@@ -76,32 +76,32 @@ self.__precacheManifest = [
     "revision": "208a394a0c11363aed45f1c184c1203c"
   },
   {
-    "url": "assets/js/7.9697af39.js",
-    "revision": "175403583a18cf24911ca74b574ba251"
+    "url": "assets/js/7.b8320bc4.js",
+    "revision": "86f3bb47bd459ad55254fb1bdf952999"
   },
   {
-    "url": "assets/js/8.0c500e35.js",
-    "revision": "f7cf55710e2a5f5a71366aa0c7ea2e03"
+    "url": "assets/js/8.dea036d2.js",
+    "revision": "85dbbc117394057fd491a0bec35fe8ed"
   },
   {
     "url": "assets/js/9.8cb24783.js",
     "revision": "c3b0ef2185e7aa3feb433b83a35a53f7"
   },
   {
-    "url": "assets/js/app.a2fa72b7.js",
-    "revision": "7afa4f265cb5e169e3b414142c776fd5"
+    "url": "assets/js/app.f51d3a57.js",
+    "revision": "d066b915b5a1a82f5df1d71197378de9"
   },
   {
     "url": "category/index.html",
-    "revision": "c0ec5ee7258146ed64cbf61932628ed2"
+    "revision": "2fbeb4307b33f028914a624cc5425295"
   },
   {
     "url": "index.html",
-    "revision": "38c844be035b30f288aa81aa501946e4"
+    "revision": "8633f9944f5db358ac55d5d71e1c9c90"
   },
   {
     "url": "tag/index.html",
-    "revision": "591af2b1e7db418c7807abb69869a824"
+    "revision": "b64e0487db90e22c2a7665faf99ab7b6"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
