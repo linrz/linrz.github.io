09e2a66cf23d13711c3d8c19b8d1214fcc4980fc
linrunze <linrunze@youzan.com>
2019-04-22 14:18:27 +0800

    hotfix: delete test.md

--- service-worker.js
+++ service-worker.js
@@ -21,67 +21,67 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "2017/11/05/fastify/index.html",
-    "revision": "6aaf4825144f87435a147e24b5f2fa40"
+    "revision": "289bca6779993fb469925cc81230633b"
   },
   {
     "url": "2018/05/15/why-i-unlike-styled-components/index.html",
-    "revision": "a2f58fc26222a551387ed9f98b1ff109"
+    "revision": "2baed6e367f32eff8d5eb4eeb4a5f9d0"
   },
   {
     "url": "2019/01/12/pnp/index.html",
-    "revision": "c378ed44ea7e438ba63c2c83b98295f6"
+    "revision": "f995dc2927283e4bb71e9aaa28884ffc"
   },
   {
     "url": "2019/03/21/berry/index.html",
-    "revision": "db4a00a8d3915f16db0010cc14c27c6f"
+    "revision": "60e083c45445eb1d5639d6f39d16a9ad"
   },
   {
     "url": "2019/04/03/why-fe-do-not-write-page-tests/index.html",
-    "revision": "ffe52b19526ba25df549d6a71e036de0"
+    "revision": "982054ed2ac72cf374022da727f85b3e"
   },
   {
     "url": "404.html",
-    "revision": "303cc84bfdf9bfb6dbc78134eb21fa56"
+    "revision": "35623a975f06b8c44b8ad9053d28b2d1"
   },
   {
     "url": "about.html",
-    "revision": "9635eb424688fcc21b83f2a997c4fd0e"
+    "revision": "35b9af59fef65f88020d4191663c8415"
   },
   {
-    "url": "assets/css/0.styles.317bd212.css",
-    "revision": "9ba80b5ab9271df8365881f087406756"
+    "url": "assets/css/0.styles.76d619d9.css",
+    "revision": "993313f631472ca1350396b7d4e00777"
   },
   {
     "url": "assets/js/1.e1ad31da.js",
     "revision": "e7f42650b7d1afbcc945d14f2708d58f"
   },
   {
-    "url": "assets/js/10.608077d7.js",
-    "revision": "50c57bc9e6ab771038a8b713ec4e3d86"
+    "url": "assets/js/10.f7ed884a.js",
+    "revision": "b6a0d918c5614df169ea7f5f08206e4c"
   },
   {
-    "url": "assets/js/11.ef2c5c54.js",
-    "revision": "e4c208aa8a01744a7c8bd7b4c447c9fd"
+    "url": "assets/js/11.24a82956.js",
+    "revision": "4da9dd97c5a15487a64d7a17f15e2d68"
   },
   {
-    "url": "assets/js/12.52cf1c53.js",
-    "revision": "794eabd3fe56b1184d4605caa0e5c3b2"
+    "url": "assets/js/12.36e435f5.js",
+    "revision": "5637e2872e0068a4645fa577d335642d"
   },
   {
-    "url": "assets/js/13.7706ffad.js",
-    "revision": "3425ffdf7f940ae8194414ab64d4ad94"
+    "url": "assets/js/13.f09dc446.js",
+    "revision": "4936bf5520751d3cfea2727dd1964f73"
   },
   {
     "url": "assets/js/14.b81f346d.js",
     "revision": "7c0a1ca24a4095895d295432e463af2f"
   },
   {
-    "url": "assets/js/3.f7b3f5da.js",
-    "revision": "6ef9099ab69cc9fac5ac36203cfd9ea0"
+    "url": "assets/js/3.7cf7fce4.js",
+    "revision": "c286908a8be36587a680cfd76f008380"
   },
   {
-    "url": "assets/js/4.61bf351f.js",
-    "revision": "127ed00667b784ac109711f94d7fb2ae"
+    "url": "assets/js/4.2bdf8611.js",
+    "revision": "067c704a2435d7570cf891ea169f12fa"
   },
   {
     "url": "assets/js/5.1c548fdf.js",
@@ -92,32 +92,32 @@ self.__precacheManifest = [
     "revision": "c8a617f895811aa64bab4daffeac8c86"
   },
   {
-    "url": "assets/js/7.e0c57542.js",
-    "revision": "18290c3cc178babee886688764fc61f4"
+    "url": "assets/js/7.4a0cc43c.js",
+    "revision": "1ee35b9e272878b9b71890358ecd1acb"
   },
   {
-    "url": "assets/js/8.92050c65.js",
-    "revision": "ed0103b9b9e35d65a99b33060d074e24"
+    "url": "assets/js/8.3d7e0c42.js",
+    "revision": "ec2b070a8b8c104b619ea4fca9ede968"
   },
   {
     "url": "assets/js/9.7380e416.js",
     "revision": "0a842726c50c0c1cf02106ca719332cb"
   },
   {
-    "url": "assets/js/app.1e5ad27b.js",
-    "revision": "4d9f9b8079e073996c1a202cad502e31"
+    "url": "assets/js/app.04ce1edd.js",
+    "revision": "dbd6c945a15d7b7e575102c321067d9b"
   },
   {
     "url": "category/index.html",
-    "revision": "7c2b5649e697457dbddf2a3413f2b8b8"
+    "revision": "2935e4671a727df77ed5de25280d1f44"
   },
   {
     "url": "index.html",
-    "revision": "39eac6b4e13ff4f51677ed46a796a9fd"
+    "revision": "3450c63fbcd3929973b61a1d48ae6116"
   },
   {
     "url": "tag/index.html",
-    "revision": "5a9a82aa9a002abbc6baefb8cdc5c7ad"
+    "revision": "babb82e25736421654a53a8050703592"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
